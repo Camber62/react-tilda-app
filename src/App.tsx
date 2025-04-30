bc0ecb267a88a71e7dd8a492f05839194b9247bd
Cambe <zakharkin@cdo-global.ru>
2025-04-30 11:30:30 +0300

    Обновлены пути к основным JS файлам в манифесте активов и HTML, добавлена логика для обработки события TriggerModalEvent в компоненте App, удалены устаревшие файлы сборки, включая старые маппинги и лицензии.

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,12 @@ function App() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
-        console.log('TriggerModalEvent 123 ');
         document.addEventListener('TriggerModalEvent', (event) => {
-            console.log('TriggerModalEvent', event);
+            setIsOpen(true);
         });
     }, []);
 
-    useEffect(() => {
-        const button = document.querySelector('a.t-btn.t142__submit[data-tilda-event-name="/tilda/click/rec995174706/button1"]');
 
-        if (button) {
-            const handleClick = (event: Event) => {
-                event.stopPropagation();
-                setIsOpen(prev => !prev);
-                console.log('Клик по кнопке: /tilda/click/rec995174706/button1');
-            };
-
-            button.addEventListener('click', handleClick);
-
-            return () => {
-                button.removeEventListener('click', handleClick);
-            };
-        }
-    }, []);
 
     return (
         <>
