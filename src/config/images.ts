da0a44d683779811245c70bbba8db2cf7aab52f5
Cambe <zakharkin@cdo-global.ru>
2025-05-13 08:54:58 +0300

    Обновлены пути к изображениям в конфигурационном файле, изменены ссылки на основные JS файлы в манифесте активов и HTML. Добавлены новые изображения в сборку и удалены устаревшие файлы.

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,8 +1,8 @@
 export const images = {
-  Frame3: '/react-tilda-app/Frame3.png',
-  Group12: '/react-tilda-app/Group12.png',
-  Group9: '/react-tilda-app/Group9.png',
-  Group10: '/react-tilda-app/Group10.png',
-  Group11: '/react-tilda-app/Group11.png',
-  Group8: '/react-tilda-app/Group8.png',
+  Frame3: '/react-tilda-app/images/Frame3.png',
+  Group12: '/react-tilda-app/images/Group12.png',
+  Group9: '/react-tilda-app/images/Group9.png',
+  Group10: '/react-tilda-app/images/Group10.png',
+  Group11: '/react-tilda-app/images/Group11.png',
+  Group8: '/react-tilda-app/images/Group8.png',
 }; 
