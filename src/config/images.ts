3635d16e25c76aee00d12aa71a7f4eb95814e692
Cambe <zakharkin@cdo-global.ru>
2025-05-13 09:29:45 +0300

    Обновлены пути к основным JS файлам в манифесте активов и HTML, удалены устаревшие файлы сборки, включая старую версию JS и LICENSE. Обновлены ссылки на изображения в конфигурационном файле.

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,8 +1,8 @@
 export const images = {
-  Frame3: '/react-tilda-app/images/Frame3.png',
-  Group12: '/react-tilda-app/images/Group12.png',
-  Group9: '/react-tilda-app/images/Group9.png',
-  Group10: '/react-tilda-app/images/Group10.png',
-  Group11: '/react-tilda-app/images/Group11.png',
-  Group8: '/react-tilda-app/images/Group8.png',
+  Frame3: 'https://Camber62.github.io/react-tilda-app/images/Frame3.png',
+  Group12: 'https://Camber62.github.io/react-tilda-app/images/Group12.png',
+  Group9: 'https://Camber62.github.io/react-tilda-app/images/Group9.png',
+  Group10: 'https://Camber62.github.io/react-tilda-app/images/Group10.png',
+  Group11: 'https://Camber62.github.io/react-tilda-app/images/Group11.png',
+  Group8: 'https://Camber62.github.io/react-tilda-app/images/Group8.png',
 }; 
